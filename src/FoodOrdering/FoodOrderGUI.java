5088321543744f4e2a8ea50b3000b17c3e37e343
Nexus-Code89 <christianleomartinez1989@gmail.com>
2022-12-17 16:46:07 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -16,6 +16,52 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    public FoodOrderGUI() {
+        btnOrder.addActionListener(e -> totalAmount());
+    }
+
+    public void totalAmount() {
+        double amount = 0.00;
+
+        if (cPizza.isSelected()) {
+            amount+= 100;
+        }
+        if (cBurger.isSelected()) {
+            amount+= 80.00;
+        }
+        if (cFries.isSelected()) {
+            amount+= 65.00;
+        }
+        if (cSoftDrinks.isSelected()) {
+            amount+= 55.00;
+        }
+        if (cTea.isSelected()) {
+            amount+= 50.00;
+        }
+        if (cSundae.isSelected()) {
+            amount+= 40.00;
+        }
+
+        if (rb5.isSelected()) {
+            double discount = amount * 0.05;
+            amount-= discount;
+            JOptionPane.showMessageDialog(panel1, String.format("The total price is Php %.2f", amount));
+        }
+        if (rb10.isSelected()) {
+            double discount = amount * 0.10;
+            amount-= discount;
+            JOptionPane.showMessageDialog(panel1, String.format("The total price is Php %.2f", amount));
+        }
+        if (rb15.isSelected()) {
+            double discount = amount * 0.15;
+            amount-= discount;
+            JOptionPane.showMessageDialog(panel1, String.format("The total price is Php %.2f", amount));
+        }
+        if (rbNone.isSelected()) {
+            JOptionPane.showMessageDialog(panel1, String.format("The total price is Php %.2f", amount));
+        }
+    }
+
     public static void main(String[] args) {
         FoodOrderGUI OrderBot = new FoodOrderGUI();
         OrderBot.setTitle("Food Ordering System");
